669966ed5d57234b9e32f82ac015de56b4989ec9
delannoazharrymuhammadf <delannoputra123@gmail.com>
2020-05-15 15:00:31 +0700

    latihan project

---
+++ LaserJet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class LaserJet : PrinterWindows
+{
+    public void Show()
+    {
+        Console.WriteLine("Laserjet display dimension : 12*12");
+    }
+    public void Print()
+    {
+        Console.WriteLine("Laserjet Printer is Printing ...");
+    }
+
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+interface PrinterWindows
+{
+    void Show();
+    void Print();
+
+}
